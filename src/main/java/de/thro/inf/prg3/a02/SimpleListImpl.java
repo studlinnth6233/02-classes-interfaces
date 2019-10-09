b2c2e0fe637ead06f3dc98642943fd5dbf3daf54
Linner-Thomas <linner.thomas@gmx.net>
2019-10-09 10:02:59 +0200

    Prototyped SimpleListImpl  - Added static inner class for Element  - Added inner class for Iterator  - Override all required methods

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,56 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable<Object>
+{
+    private static class Element
+    {
+        public Object  item;
+        public Element next;
+    }
 
-	// TODO: Implement the required methods.
+    private class SimpleIteratorImpl implements Iterator<Object>
+    {
+        @Override
+        public boolean hasNext()
+        {
+            return false;
+        }
+        @Override
+        public Object next()
+        {
+            return null;
+        }
+    }
 
+    private Element head;
+
+    @Override
+    public Iterator<Object> iterator()
+    {
+        return new SimpleIteratorImpl();
+    }
+
+    @Override
+    public void add(Object o)
+    {
+
+    }
+
+    @Override
+    public int size()
+    {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter)
+    {
+        return null;
+    }
 }
--- src/test/java/de/thro/inf/prg3/a02/tests/SimpleListTest.java
+++ src/test/java/de/thro/inf/prg3/a02/tests/SimpleListTest.java
@@ -12,57 +12,68 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListTest {
+public class SimpleListTest
+{
 
-	private SimpleListImpl testList;
+    private SimpleListImpl testList;
 
-	@BeforeEach
-	void setup(){
-		testList = new SimpleListImpl();
+    @BeforeEach
+    void setup()
+    {
+        testList = new SimpleListImpl();
 
-		testList.add(1);
-		testList.add(2);
-		testList.add(3);
-		testList.add(4);
-		testList.add(5);
-	}
+        testList.add(1);
+        testList.add(2);
+        testList.add(3);
+        testList.add(4);
+        testList.add(5);
+    }
 
-	@Test
-	void testAddElements(){
-		int counter = 0;
-		for(Object o : testList){
-			counter++;
-		}
-		assertEquals(5, counter);
-	}
+    @Test
+    void testAddElements()
+    {
+        int counter = 0;
+        for (Object o : testList)
+        {
+            counter++;
+        }
+        assertEquals(5, counter);
+    }
 
-	@Test
-	void testSize(){
-		assertEquals(5, testList.size());
-	}
+    @Test
+    void testSize()
+    {
+        assertEquals(5, testList.size());
+    }
 
-	@Test
-	void testFilterAnonymousClass(){
-		SimpleListImpl result = (SimpleListImpl) testList.filter(new SimpleFilter() {
-			@Override
-			public boolean include(Object item) {
-				int current = (int)item;
-				return current > 2;
-			}
-		});
+    @Test
+    void testFilterAnonymousClass()
+    {
+        SimpleListImpl result = (SimpleListImpl) testList.filter(new SimpleFilter()
+        {
+            @Override
+            public boolean include(Object item)
+            {
+                int current = (int) item;
+                return current > 2;
+            }
+        });
 
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i > 2);
-		}
-	}
+        for (Object o : result)
+        {
+            int i = (int) o;
+            assertTrue(i > 2);
+        }
+    }
 
-	@Test
-	void testFilterLambda(){
-		SimpleListImpl result = (SimpleListImpl) testList.filter(o -> ((int)o) % 2 == 0);
-		for(Object o : result){
-			int i = (int)o;
-			assertTrue(i % 2 == 0);
-		}
-	}
+    @Test
+    void testFilterLambda()
+    {
+        SimpleListImpl result = (SimpleListImpl) testList.filter(o -> ((int) o) % 2 == 0);
+        for (Object o : result)
+        {
+            int i = (int) o;
+            assertTrue(i % 2 == 0);
+        }
+    }
 }
